c6f06197129f54d77d1b3c50b3fd88c9067d4f96
Shivji <shivji7448@gmail.com>
2019-05-02 18:05:54 +0530

    new (usr,pub,art) functions created

--- truffle/contracts/AccountManagment.sol
+++ truffle/contracts/AccountManagment.sol
@@ -2,33 +2,46 @@ pragma solidity ^0.5.0;
 
 contract AccountManagment {
     
-    struct article{
-        string artHash;
-        address artId;
-        address author;
-        address publisher;
-        string link;
-        string timestamp;
+    struct node{
+        address owner;
     }
-    
-    struct publisher{
-        address pubId;
-        string name;
-        string webAddress;
-        mapping (address => bool) myAuthors;
-    }
+    mapping (address => node) nod;
     
     struct user{
-        address usrId;
         string name;
         string email;
         uint numPublisher;
         mapping (uint => address) myPublishers;
     }
+    mapping (address => user) usr;
     
-    struct node{
-        address nodeId;
-        address owner;
+    struct publisher{
+        string name;
+        string webAddress;
+        mapping (address => bool) myAuthors;
+    }
+    mapping(address => publisher) pub;
+    
+    struct article{
+        string artHash;
+        address author;
+        address publisher;
+        string link;
+        string timestamp;
+    }
+    mapping (address => article) art;
+    
+
+    function newUser(address _uid, string memory _name, string memory _email) public{
+        usr[_uid] = user({name: _name, email : _email, numPublisher : 0});
+    }
+    
+    function newPublisher(address _pid, string memory _name, string memory _webAddr) public{
+        pub[_pid] = publisher({name: _name, webAddress : _webAddr});
+    }
+    
+    function newArticle(address _artid, string memory _aHash, string memory _link, address _auth, address _pub, string memory _tmstamp) public{
+        art[_artid] = article({artHash: _aHash, author: _auth, publisher: _pub, link: _link, timestamp: _tmstamp });
     }
     
 }
