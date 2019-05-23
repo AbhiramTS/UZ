9e9dcaa555db5e21e16ef577cca53e67616e4f9a
abhiramTS <abhiramts007@gmail.com>
2019-05-23 12:45:00 +0530

    Added template component

--- UI/src/app/app.module.ts
+++ UI/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +10,8 @@ import {Web3ServiceService} from './services/web3-service.service';
 import { NavbarComponent } from './navbar/navbar.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { LoginComponent } from './login/login.component';
+import { TemplatingComponent } from './templating/templating.component';
+import { SafePipe } from './safe.pipe';
 
 @NgModule({
   declarations: [
@@ -16,11 +19,14 @@ import { LoginComponent } from './login/login.component';
     AccountComponent,
     NavbarComponent,
     RegistrationComponent,
-    LoginComponent
+    LoginComponent,
+    TemplatingComponent,
+    SafePipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [
     Web3ServiceService
