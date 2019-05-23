62e9ad1592c37df5a25d7a9dcb14cb2150178d85
Shivji <shivji7448@gmail.com>
2019-05-23 21:49:02 +0530

    articledrafter UI 1.0

--- UI/src/app/drafter/drafter.component.ts
+++ UI/src/app/drafter/drafter.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: 'app-drafter',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DrafterComponent implements OnInit {
 
-  constructor() { }
+  private myTmplt : any = "loading...";
+  @Input('link') link: string ;
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.http.get(this.link,{responseType: 'text'}).subscribe((data: any ) => {
+      this.myTmplt = data;
+    });
   }
 
 }
